b578ee5fe45f1607442d48681a2b0408355474e1
Tomasz <tomasz.zak.za@gmail.com>
2018-10-02 22:02:51 +0200

    temporary saving of json for inspecting

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ def save_current_app_list_to_csv(csv_name):
     all_apps_url = "https://api.steampowered.com/ISteamApps/GetAppList/v0001/"
     current_players_per_app = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?format=json&appid='
 
-    r = requests.get(all_apps_url)
-    j = json.loads(r.text)
-    applist = j['applist']['apps']['app']
+    response = requests.get(all_apps_url)
+    json_response = json.loads(response.text)
+    with open('json_resp.json', 'w') as f:
+		f.write(response.text.encode('utf-8'))
+
+    applist = json_response['applist']['apps']['app']
 
     with open(csv_name, 'wb') as f:
         g = csv.writer(f)
@@ -27,8 +30,7 @@ def read_key_from_file():
 def main():
     save_current_app_list_to_csv(str(datetime.datetime.now()).replace(":", "-")+'.csv')
 
-
 if __name__ == '__main__':
-    main()
+	pass
+    #main()
 
-class
--- steamReq.py
+++ steamReq.py
@@ -1,7 +1,23 @@
 class SteamReq():
-	def __init__(self, *args):
-	main_url = "https://apiesteampowered.com/ISteamApps/" # GetAppList/v0001/":
-	API_version = 'v1'
-	def __enter__():
-		import requests as re
+	def __init__(self, url):
+		print("Init klasy nadrzednej")
+		#<cr>main_url = "https://apiesteampowered.com/ISteamApps/" # GetAppList/v0001/":
+		#<cr>API_version = 'v1'
+		#<cr>my_url = url
 
+
+	def print_count():
+		print(requests_count)
+
+
+	def increase_count():
+		SteamReq.requests_count+=1
+	
+	def make_request():
+		pass
+
+
+class AllAppsReq(SteamReq):
+	def __init__(self):
+		super(AllAppsReq, self).__init__('mamma mia')
+		print("Init klasy pochodnej")
