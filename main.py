49c74fcf2c7763f32013026ec20e8fee79c2991e
Tomasz <tomasz.zak.za@gmail.com>
2018-10-11 22:02:08 +0200

    drafting download of the current players

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ def save_current_app_list_to_csv(csv_name):
 
     response = requests.get(all_apps_url())
     json_response = json.loads(response.text)
-    with open('json_resp.json', 'w') as f:
-		f.write(response.text.encode('utf-8'))
-
     applist = json_response['applist']['apps']['app']
 
     with open(csv_name, 'wb') as f:
@@ -18,14 +15,26 @@ def save_current_app_list_to_csv(csv_name):
         for app in applist:
             g.writerow([app['appid'], app['name'].encode('utf-8')])
 
-
 def read_key_from_file():
     with open('steamKey.txt', 'r') as f:
         steam_key = f.readline()
     return steam_key
 
+def save_current_players_count_to_csv(csv_name, app_id):
+	import requests, json, csv
+	from steamAPINames import current_players
+	response = requests.get(current_players(app_id))
+	json_response = json.loads(response.text)
+	
+	with open(csv_name, 'wb') as f:
+		g = csv.writer(f)
+		g.writerow(['AppId', 'PlayerCount','Date'])
+		g.writerow([app_id, json_response['response']['player_count'],
+		str(datetime.datetime.now())])
+
 def main():
-    save_current_app_list_to_csv(str(datetime.datetime.now()).replace(":", "-")+'.csv')
+	pass
+    #save_current_app_list_to_csv(str(datetime.datetime.now()).replace(":", "-")+'.csv')
 
 if __name__ == '__main__':
     main()
